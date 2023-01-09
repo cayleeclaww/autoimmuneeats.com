be947d7f083699a1a147a8a294418814f357baac
Caylee Claww <ninjahead@gmail.com>
2023-01-09 06:49:54 -0500

    Updated PHP to include automatically populate links to new posts on main page, cleaned up CSS

--- front-page.php
+++ front-page.php
@@ -4,6 +4,8 @@
 // Fetch theme header template
 get_header(); ?>
 
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,15 +19,86 @@ get_header(); ?>
 <body>
     <div class="article-title">Latest</div>
     <div class="recent-articles-container">
-        <div class="recent-article">Recent Article One</div>
-        <div class="recent-article">Recent Article Two</div>
-        <div class="recent-article">Recent Article Three</div>
+        <div class="recent-article">
+            <!-- Article #1 -->
+        
+            <?php 
+            $the_query = new WP_Query( array( 'posts_per_page' => 1,'offset' => 1 ) ); 
+            ?>
+
+            <!-- // Start our WP Query -->
+            <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+
+            <!-- // Display the Post Title with Hyperlink -->
+            <p><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></p>
+
+            <?php 
+            endwhile;
+            wp_reset_postdata();
+            ?>
+        </div>
+
+
+
+        <div class="recent-article">
+            <!-- Article #2 -->
+         <?php 
+             $the_query = new WP_Query( array( 'posts_per_page' => 1,'offset' => 2 ) ); 
+            ?>
+
+            <!-- // Start our WP Query -->
+            <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+
+            <!-- // Display the Post Title with Hyperlink -->
+            <p><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></p>
+
+            <?php 
+            endwhile;
+            wp_reset_postdata();
+            ?>
+
+
+        </div>
+        <div class="recent-article">
+            <!-- Article #3 -->
+            <?php 
+             $the_query = new WP_Query( array( 'posts_per_page' => 1,'offset' => 3 ) ); 
+            ?>
+
+            <!-- // Start our WP Query -->
+            <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+
+            <!-- // Display the Post Title with Hyperlink -->
+            <p><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></p>
+
+            <?php 
+            endwhile;
+            wp_reset_postdata();
+            ?>
+        </div>
     </div>
     <div class="page-break"></div>
     <div class="article-title">Featured</div>
     <div class="featured">
         <div class="featured-photo">Featured Article Photo</div>
-        <div class="featured-article">Featured Article Title</div>
+        <div class="featured-article">
+            <!-- Featured Article Title -->
+        
+            <?php 
+            $the_query = new WP_Query( array( 'posts_per_page' => 1,'offset' => 1 ) ); 
+            ?>
+
+            <!-- // Start our WP Query -->
+            <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+
+            <!-- // Display the Post Title with Hyperlink -->
+            <p><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></p>
+
+            <?php 
+            endwhile;
+            wp_reset_postdata();
+            ?>
+        </div>
     </div>
     <div class="page-break"></div>
 
@@ -40,18 +113,15 @@ get_header(); ?>
     <div class="page-break"></div>
 
 
-
     <div class="article-title">Explore</div>
     <div class="popular-articles-container">
-        <div class="sub-container">
-            <div class="popular-article">Popular Article One</div>
-            <div class="caption">Category 1</div>
-        </div> 
+        <div class="popular-article">Popular Article One</div>
         <div class="popular-article">Popular Article Two</div>
         <div class="popular-article">Popular Article Three</div>
         <div class="popular-article">Popular Article Four</div>
     </div>
     <div class="page-break"></div>
+
 
 
 
